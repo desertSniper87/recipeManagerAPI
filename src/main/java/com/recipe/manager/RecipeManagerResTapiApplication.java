5a46299b9dd36c91cfd662cbcff1d247aab13fca
desertsniper87 <torshobuet@gmail.com>
2019-12-23 16:16:10 +0600

    initial commit

---
+++ src/main/java/com/recipe/manager/RecipeManagerResTapiApplication.java
@@ -0,0 +1,13 @@
+package com.recipe.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeManagerResTapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeManagerResTapiApplication.class, args);
+	}
+
+}
